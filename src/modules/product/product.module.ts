243ed2a8be9ce848dea04b15e4147cdd02b823fa
Pierre Garamendi <pierre.garamendi@gmail.com>
2023-07-13 09:37:58 -0500

    product module changed

--- src/modules/product/product.module.ts
+++ src/modules/product/product.module.ts
@@ -5,17 +5,10 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { Product, productSchema } from 'src/modules/product/schema/product.schema';
 
 @Module({
-  imports:[
-    MongooseModule.forFeature(
-      [
-          {
-              name: Product.name,
-              schema: productSchema
-          }
-      ]
-  )
+  imports: [
+    MongooseModule.forFeature([{ name: Product.name, schema: productSchema }])
   ],
   controllers: [ProductController],
   providers: [ProductService]
 })
-export class ProductModule {}
+export class ProductModule { }
